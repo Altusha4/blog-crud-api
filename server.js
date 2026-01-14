9c5481d6dda183213c9d1aaec33ee7cdc03411cf
Алтынай Ертай <altynayyertay@MacBook-Air-Altynaj.local>
2026-01-14 19:39:06 +0500

    Fix app initialization before using routes

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const express = require('express')
 const http = require("node:http");
+const blogRoutes = require("./routes/blogRoutes");
+
 const app = express()
 
 const DB_URI = process.env.MONGO_URI;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use("/blogs", blogRoutes)
 app.get('/', (req, res) => {
     res.send("works!")
 })
 mongoose.connect(DB_URI)
     .then(() => {
         console.log("Connected")
-
         app.listen(3000, () => console.log("http://localhost:3000"))
     })
+    .catch(err => console.error(err))
