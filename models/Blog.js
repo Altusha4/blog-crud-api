3663830ae3fbc34d762cb1bf298c5de922e5d0b3
Алтынай Ертай <altynayyertay@MacBook-Air-Altynaj.local>
2026-01-22 15:01:19 +0500

    feat: add Blog model with title, body and author schema

--- models/Blog.js
+++ models/Blog.js
@@ -2,23 +2,11 @@ const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema(
     {
-        title: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        body: {
-            type: String,
-            required: true
-        },
-        author: {
-            type: String,
-            default: "Anonymous"
-        }
+        title: { type: String, required: true, trim: true },
+        body: { type: String, required: true },
+        author: { type: String, default: "Anonymous" }
     },
-    {
-        timestamps: true
-    }
+    { timestamps: true }
 );
 
 module.exports = mongoose.model("Blog", blogSchema);
